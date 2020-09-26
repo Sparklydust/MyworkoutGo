12d54048364a6068167f791c11bb7d842dfcaad7
Roland Lariotte <roland.sound@live.fr>
2020-09-27 02:52:13 +0300

    Add NetworkEnpoint.swift to handle urls from MyworkoutGOServerSide

---
+++ MyworkoutGO/Enums/NetworkEndpoint.swift
@@ -0,0 +1,38 @@
+//
+//  NetworkEndpoint.swift
+//  MyworkoutGO
+//
+//  Created by Roland Lariotte on 27/09/2020.
+//
+
+import Foundation
+
+//  MARK: NetworkEndpoint
+/// MyworkoutGO network url endpoints enumaration.
+///
+/// All urls are handled by this enum with a base url
+/// and endpoints for earch network call.
+///
+enum NetworkEndpoint {
+
+  /// Base api url.
+  ///
+  static let baseURL = URL(string: "http://localhost:8080/api/users/")!
+
+  case logIn
+  case signUp
+  case account
+
+  /// Fetching MyworkoutGO endpoint.
+  ///
+  var url: URL {
+    switch self {
+    case .logIn:
+      return NetworkEndpoint.baseURL.appendingPathComponent("login")
+    case .signUp:
+      return NetworkEndpoint.baseURL.appendingPathComponent("signup")
+    case .account:
+      return NetworkEndpoint.baseURL.appendingPathComponent("account")
+    }
+  }
+}
